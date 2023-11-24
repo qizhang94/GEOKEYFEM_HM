a93a4851e2171ddbc1dcd4b2ad0b44e78d88c805
qizhang94 <qzhang94@stanford.edu>
2023-11-24 15:33:12 +0800

    Update GasHM

--- GasHM/main_2d_shale_gasflow.m
+++ GasHM/main_2d_shale_gasflow.m
@@ -1,5 +1,5 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Modified integration force is changed to 增量 (u and p)            %
+% Modified integration force is changed to p and incremental u
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 restoredefaultpath;
@@ -56,6 +56,7 @@ hfun = @hfun8;
     tria,~] = smooth2(vert,etri,tria,tnum);
 
 % figure;
+% set(gcf,'Color','w');
 % patch('faces',tria(:,1:3),'vertices',vert, ...
 %     'facecolor','w', ...
 %     'edgecolor',[.2,.2,.2]);
@@ -64,8 +65,9 @@ hfun = @hfun8;
 %     'facecolor','w', ...
 %     'edgecolor',[.1,.1,.1], ...
 %     'linewidth',1.5);
-% title(['MESH-OPT.: KIND=DELFRONT, |TRIA|=', ...
-%     num2str(size(tria,1))]);
+% title(['MESH-OPT.: KIND=DELFRONT, $\rm \|TRIA\|$=', ...
+%     num2str(size(tria,1))],'Interpreter','latex','fontsize',13);
+% exportgraphics(gcf,'./gas_data/FE_mesh.pdf','ContentType','vector');
 
 gcoord = vert; ele_nods = tria;
 
@@ -86,6 +88,10 @@ K = 2.8662e+03; nu = 0.15; tau = 0; k_perm = 2e-19; mu_f = 2e-11; rho_buo = 0; p
 data_const.tauG = tau/(3*K*(1 - 2*nu)/1/(1+nu));   % PPP Stabilization multiplier, already divided by 2G
 data_const.mobility = k_perm/mu_f;
 data_const.poro = poro; % Porosity appears in the gas compressibility term
+data_const.rhorock = 0; % kg/m^3
+data_const.V_L = 0.015; % m^3/kg
+data_const.P_L = 4; % MPa
+data_const.T = 353.15; % Reservoir temperature [K]
 
 % cohesion = 100; phi = pi/3; psi = pi/3; % make it elastic
 Props = ones(nnode, 1)*[1.07, 0.01, 0, pi/6, poro]; % pi/6 is the bedding orientation
@@ -122,10 +128,11 @@ traction_xL = @(x,t)(F_x); traction_xR = @(x,t)(-F_x);
 traction_yB = @(x,t)(F_y); traction_yT = @(x,t)(-F_y);
 
 
-watch.dt = [10*ones(1,10), 10*(1.5.^(1:1:30))];   % time STEP INTERVAL
+watch.dt = [10*ones(1,10), 10*(1.5.^(1:1:30)), 30*24*3600*ones(1,15)];   % time STEP INTERVAL
 watch.now = 0; % start
 bcval_incr = sparse(length(watch.dt), length(bcdof));
 epsp = 1;
+watch.max = 500*24*3600; % Maximum simulation time 500 days
 
 %% For contact model
 
@@ -149,6 +156,7 @@ max_newton_iter = 100; tol = 1e-3; tol2 = 1e-7;
 
 cellstress = cell(length(watch.dt), 1); % Store stress tensor on every node for all time steps
 cellUP = cell(length(watch.dt), 1); % Store the "new_solution" for all time steps
+cellSDV = cell(length(watch.dt), 1);
 stress = in_situ_stress*ones(1,nnode);  % Old
 stress_new = stress;
 SDV = zeros(2,nnode); % Solution-Dependent State Variables
@@ -158,6 +166,10 @@ SDV_new = SDV;
 for step = 1:length(watch.dt)
     watch.now = watch.now + watch.dt(step);
     delta_t  = watch.dt(step);
+    if watch.now > watch.max
+        delta_t = delta_t - (watch.now - watch.max);
+        watch.now = watch.max;
+    end     
     print_info = 'LOAD STEP = %d; TIME = %.2f:\n';
     fprintf(print_info, step, watch.now);
     residual_traction = assign_tractionBC2(traction_temp, b_right, traction_xR, watch.now, 'vertical') + ...
@@ -210,6 +222,7 @@ for step = 1:length(watch.dt)
     SDV = SDV_new;
     cellstress{step} = stress;
     cellUP{step} = old_solution;
+    cellSDV{step} = SDV;
     
 end
 
@@ -220,15 +233,18 @@ clearvars new_solution_temp
 
 %% Plot and post-processing
 
+toc;
+
 elemType = 'T3';
 
 dispNodes = b_bottom;
 dispNodes1=union(b_left, b_right);
 
+plotstep = 40; % 66.5810 days
 
-u_xp = new_solution(1:2:nnode*ndof-1);
-u_yp = new_solution(2:2:nnode*ndof);
-p_nodal = new_solution(nnode*ndof+1:end);
+u_xp = cellUP{plotstep}(1:2:nnode*ndof-1);
+u_yp = cellUP{plotstep}(2:2:nnode*ndof);
+p_nodal = cellUP{plotstep}(nnode*ndof+1:end);
 
 node = gcoord;
 element = ele_nods;
@@ -239,7 +255,7 @@ hold on
 plot_mesh(node,element,elemType,'k-');
 plot(node(dispNodes,1),node(dispNodes,2),'ks');
 plot(node(dispNodes1,1),node(dispNodes1,2),'ko');
-title('Undeformed FE mesh')
+title('Undeformed FE mesh');
 
 % Plot numerical deformed configuration
 fac=1;         
@@ -247,7 +263,7 @@ figure
 clf
 hold on
 plot_mesh(node+fac*[u_xp u_yp],element,elemType,'k-');
-title(' Numerical deformed mesh')
+title(' Numerical deformed mesh');
 plot(node(dispNodes,1),node(dispNodes,2),'ks');
 plot(node(dispNodes1,1),node(dispNodes1,2),'ko');
 hold on
@@ -256,65 +272,107 @@ hold on
 % Plot deformation intensity with a colormap
 figure
 clf
+set(gcf,'Color','w');
 subplot(2,1,1);
 plot_field(node+fac*[u_xp u_yp],element,elemType,u_xp);
-colorbar
-title('Deformation plot, U_X, m')
+c = colorbar; c.FontSize = 13; set(c,'TickLabelInterpreter','latex');
+set(gca,'TickLabelInterpreter','latex');
+set(gca,'FontSize',15);
+title('Deformation plot, $U_X$, m','Interpreter','latex','fontsize',15);
 
 subplot(2,1,2);
 plot_field(node+fac*[u_xp u_yp],element,elemType,u_yp);
-colorbar
-title('Deformation plot, U_Y, m')
+c = colorbar; c.FontSize = 13; set(c,'TickLabelInterpreter','latex');
+set(gca,'TickLabelInterpreter','latex');
+set(gca,'FontSize',15);
+title('Deformation plot, $U_Y$, m','Interpreter','latex','fontsize',15);
+
+exportgraphics(gcf,'./gas_data/disp_field_step40.pdf','ContentType','vector');
 
 % Plot stress with a colormap
 % MPa
-t1 = stress(1,:);
-t2 = stress(2,:);
-t3 = stress(4,:);
-t4 = stress(3,:);
+t1 = cellstress{plotstep}(1,:);
+t2 = cellstress{plotstep}(2,:);
+t3 = cellstress{plotstep}(4,:);
+t4 = cellstress{plotstep}(3,:);
 
 FontSize=16;
 hh=figure('Name','Stress');
 set(hh,'color','w');
 % set(hh,'Position',[400 100 1000 800]);
 subplot(2,2,1);
-snscontour(node, element,fac, u_xp,u_yp,t1','{\it\sigma}_{xx}');
+snscontour(node, element,fac, u_xp,u_yp,t1','{\it\sigma''}_{xx}');
 set(gca,'Fontsize',FontSize,'FontName','Times new Roman');
 hold on
 
 
 subplot(2,2,2);
-snscontour(node, element,fac, u_xp,u_yp,t2','{\it\sigma}_{yy}');
+snscontour(node, element,fac, u_xp,u_yp,t2','{\it\sigma''}_{yy}');
 set(gca,'Fontsize',FontSize,'FontName','Times new Roman');
 hold on
 
 subplot(2,2,3);
-snscontour(node, element,fac, u_xp,u_yp,t3','{\it\sigma}_{xy}');
+snscontour(node, element,fac, u_xp,u_yp,t3','{\it\sigma''}_{xy}');
 set(gca,'Fontsize',FontSize,'FontName','Times new Roman');
 hold on
 
 subplot(2,2,4);
-snscontour(node, element,fac, u_xp,u_yp,t4','{\it\sigma}_{zz}');
+snscontour(node, element,fac, u_xp,u_yp,t4','{\it\sigma''}_{zz}');
 set(gca,'Fontsize',FontSize,'FontName','Times new Roman');
 hold on
-sgtitle('Stress tensor components, MPa');
+sgtitle('Terzaghi effective stress {\boldmath$\sigma$''} components, MPa','Interpreter','latex','fontsize',15);
+
+exportgraphics(gcf,'./gas_data/stress_field_step40.pdf','ContentType','vector');
 
 
 % Plot equivalent plastic strain with a colormap
 fac = 1;
 figure
-t5=SDV(2,:);
-snscontour(node, element,fac, u_xp,u_yp,t5','PEEQ');
-title('Equivalent plastic strain')
+set(gcf,'Color','w');
+t5=cellSDV{plotstep}(2,:);
+snscontour(node,element,fac, u_xp,u_yp,t5','PEEQ');
+c = colorbar; c.FontSize = 14; set(c,'TickLabelInterpreter','latex');
+title('Equivalent plastic strain','Interpreter','latex','fontsize',15);
 hold on
+
+exportgraphics(gcf,'./gas_data/peeq_field_step40.pdf','ContentType','vector');
+
 
 % Pore pressure
 figure
-snscontour(node, element,fac, u_xp,u_yp,p_nodal,'PP');
-title('Pore pressure, MPa')
+set(gcf,'Color','w');
+snscontour(node,element,fac,u_xp,u_yp,p_nodal,'PP');
+c = colorbar; c.FontSize = 14; set(c,'TickLabelInterpreter','latex');
+title('Gas pressure $p$, MPa','Interpreter','latex','fontsize',15);
 hold on
 
+exportgraphics(gcf,'./gas_data/gasp_field_step40.pdf','ContentType','vector');
+
+
+p_at_corner = zeros(length(watch.dt),2);
+time = 0;
+% Gas pressure at corner
+for step = 1:length(watch.dt)
+    time = time + watch.dt(step);
+    p_at_corner(step,:) = [time/24/3600, cellUP{step}(nnode*ndof+1)]; % (-1, -1) POINT; (1, 0) POINT: 135
+end
+
+figure;
+set(gcf,'Color','w');
+semilogx(p_at_corner(:,1),p_at_corner(:,2), 'b', 'Linewidth', 1); grid on; hold on;
+% ref = load('./gas_data/reference_result.mat');
+% semilogx(ref.COMSOL_ELASTIC(:,1),ref.COMSOL_ELASTIC(:,2), 'r-.', 'Linewidth', 1);
+xlim([0.1, 500]);
+xlabel('Time (d)', 'Interpreter','latex');
+ylabel('Gas $p$ (MPa)', 'Interpreter','latex');
+set(gca,'TickLabelInterpreter','latex');
+set(gca, 'FontSize', 13);
+set(get(gca, 'XLabel'),'FontSize', 15);
+set(get(gca, 'YLabel'),'FontSize', 15);
+
 toc;
+
+% save ./gas_data/reference_result.mat COMSOL_ELASTIC p_at_corner;
 
 %% user-defined mesh function
 function [hfun] = hfun8(test)
